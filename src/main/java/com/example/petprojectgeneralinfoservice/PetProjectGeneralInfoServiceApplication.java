394de2ef03cade00e977498d04769d29a7e54469
kiorescu <chiorescu.marin@gmail.com>
2025-04-07 19:11:44 +0300

    Initial commit

---
+++ src/main/java/com/example/petprojectgeneralinfoservice/PetProjectGeneralInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.petprojectgeneralinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetProjectGeneralInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetProjectGeneralInfoServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petprojectgeneralinfoservice/PetProjectGeneralInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petprojectgeneralinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectGeneralInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
