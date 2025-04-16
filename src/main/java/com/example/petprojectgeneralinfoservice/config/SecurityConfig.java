a87544cc474f46b9f8f2a16223f613741077f086
kiorescu <chiorescu.marin@gmail.com>
2025-04-16 18:26:08 +0300

    Security config - disable basic auth

--- src/main/java/com/example/petprojectgeneralinfoservice/config/SecurityConfig.java
+++ src/main/java/com/example/petprojectgeneralinfoservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,7 +13,8 @@ public class SecurityConfig{
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        http.httpBasic(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
